886b0d0bb2eb7bb888f87225bbebcd7c439401fd
Vit Kocurek <Vit Kocurek>
2017-01-12 19:57:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+mine
+====
+
+A Symfony project created on January 12, 2017, 6:27 pm.
---
+++ src/AppBundle/Controller/TodoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * @Route("/todos", name="todo_list")
+     */
+    public function listAction(Request $request) {
+        // replace this example code with whatever you need
+        return $this->render('todo/index.html.twig'
+        );
+    }
+}
